8a4199720099e333a207d4bb614f353c005446bb
Hugo Martinez <cousinlemon@gmail.com>
2024-10-31 19:23:57 -0500

    optimizado y actualizado URL

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-d9901281-2da2-439c-bd1d-1370206591ef.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-8434ea1a-25b8-45c1-b3d9-68c4844fc73f.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
